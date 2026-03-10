b5f48fbfdeda510c351b30952d25d46a77196f30
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-10 09:11:21 +0300

    Understanding Data types

---
+++ Data_types.py
@@ -0,0 +1,50 @@
+#integer
+my_integer_var = 10
+print('Integer:', my_integer_var)
+print(type(my_integer_var))
+
+#float
+my_float_var = 4.50
+print('Float:', my_float_var) 
+print(type(my_float_var))
+
+#string
+my_string_var = 'hello'
+print('String :', my_string_var)
+print(type(my_string_var))
+
+#boolean
+my_boolean_var = True
+print('Boolean:', my_boolean_var)
+print(type(my_boolean_var))
+
+
+#set
+my_set_var = {7,5,8}
+print('Set:', my_set_var)
+print(type(my_set_var))
+
+#dictionary
+my_dictionary_var = {'name': 'Alice', 'age':25}
+print('Dictionary:', my_dictionary_var)
+print(type(my_dictionary_var))
+
+#tuple
+my_tuple_var = (7,5,8)
+print('Tuple:', my_tuple_var)
+print(type(my_tuple_var))
+
+#range
+my_range_var = range(5)
+print('Range:', my_range_var)
+print(type(my_range_var))
+
+#list
+my_list = [22, 'Hello world', 3.14, True]
+print(my_list)
+print(type(my_list))
+
+#none 
+my_none_var = None
+print('None:', my_none_var)
+print(type(my_none_var))
