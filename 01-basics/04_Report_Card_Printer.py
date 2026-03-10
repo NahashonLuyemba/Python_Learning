4d7797c10e1ab439cce1da3e555f44ca8aeab3e0
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-10 10:57:24 +0300

    completed report card printer

--- 01-basics/04_Report_Card_Printer.py
+++ 01-basics/04_Report_Card_Printer.py
@@ -4,4 +4,10 @@ user_name = 'Alice'
 print(user_name, type(user_name))
 is_student = True
 print(is_student, type(is_student))
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score))
 
