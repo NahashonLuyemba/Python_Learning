52e464f365f052d9f878736dc33fd07d7942abe6
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-11 12:46:42 +0300

    Checking if string contains more than one character

--- 01-basics/05_Strings.py
+++ 01-basics/05_Strings.py
@@ -19,11 +19,13 @@ print(msg, quote)
 
 my_str = 'Hello world'
 
-print('Hello' in my_str)
-print('hey' in my_str)
-print('hi' in my_str)
-print('e' in my_str)
-print('f' in my_str)
+print('Hello' in my_str) #True
+print('hey' in my_str) #False
+print('hi' in my_str) #False
+print('e' in my_str) #True
+print('f' in my_str) #False
+
+
 
 
 
