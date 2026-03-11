5f224bbf3367888852a1485c5af7cc5315764241
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-11 12:27:23 +0300

    Started on strings

---
+++ 01-basics/05_Strings.py
@@ -0,0 +1,20 @@
+#String-seq of characters surrounded by either single or double quote marks.
+
+my_str_1 = 'Hello'
+my_str_2 = "World"
+print(my_str_1, my_str_2)
+
+#multiline string, use triple double quotes or single quotes
+
+my_str_3 = """Multiline
+string\n """
+my_str_4 = '''Another multiline string'''
+print(my_str_3, my_str_4)
+
+msg = "It's a sunny day"
+quote = 'She said, "Hello World!"'
+print(msg, quote)
+
+
+
+
