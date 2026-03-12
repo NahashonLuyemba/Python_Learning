5d03ddc5a8579b3f75382ed74bbce23a4b1253cc
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-12 06:02:29 +0300

    String interpolation

--- 01-basics/05_Strings.py
+++ 01-basics/05_Strings.py
@@ -71,6 +71,16 @@ name_and_age += str(age) #Append the age as string
 
 print(name_and_age) #John Doe26
 
+#String interpolation(f string)
+
+name = 'John Doe'
+age = 26
+name_and_age = f'My name is {name} and I am {age} years old'
+print(name_and_age) #My name is John Doe nad I am 26 years old
+
+num1 = 5
+num2 = 10
+print(f'The sum of {num1} and {num2} is {num1 + num2}') #The sum of 5 and 10 is 15
 
 
 
