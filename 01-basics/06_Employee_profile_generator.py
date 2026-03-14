36a0269722b61bbca7ce556ff5754b478f7951b4
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-14 22:32:05 +0300

    Updated employee info

--- 01-basics/06_Employee_profile_generator.py
+++ 01-basics/06_Employee_profile_generator.py
@@ -1,6 +1,17 @@
 first_name = 'John'
 last_name = "Doe"
-print(first_name)
-print(last_name)
 
-full_name = first_name + last_name
+
+full_name = first_name + ' ' + last_name # added space between two names
+print(full_name)
+
+address = '123 Main Street'
+address += ', Apartment 4B'
+print(address)
+
+employee_age = 28
+employee_info = full_name + " is " + str(employee_age)
+employee_info += ' years old'
+print(employee_info)
+
+
