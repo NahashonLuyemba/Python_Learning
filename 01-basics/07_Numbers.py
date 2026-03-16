e9fbd2685869bb9f6d4a40afe151d3629e0c82ae
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-16 15:53:04 +0300

    continous update on numbers and operations

--- 01-basics/07_Numbers.py
+++ 01-basics/07_Numbers.py
@@ -1,4 +1,4 @@
-# integers
+# INTEGERS:
 
 my_int_1 = 56
 my_int_2 = -4
@@ -24,3 +24,67 @@ my_int_2 = 4
 
 product_ints = my_int_1 * my_int_2
 print('Integer Multiplication:', product_ints)
+
+#division
+my_int_1 = 56
+my_int_2 = 12
+
+div_ints = my_int_1/my_int_2
+print('Integer Division:', div_ints)
+
+#FLOATS:
+
+my_float_1 = -12.0
+my_float_2 = 4.9
+
+print(type(my_float_1))
+print(type(my_float_2))
+
+#addition of floats
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+float_addition = my_float_1 + my_float_2
+print('Float Addition:', float_addition)
+
+#subtraction
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+float_subtraction = my_float_2 - my_float_1
+print('Float Subtraction:', float_subtraction)
+
+#multiplication
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+float_multiplication = my_float_2 * my_float_1
+print('Float Multiplication:', float_multiplication)
+
+#division
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+float_division = my_float_2 / my_float_1
+print('Float Division:', float_division)
+
+#addition of a float and integer
+my_int = 56
+my_float = 5.4
+sum_int_and_float = my_int + my_float
+
+print(sum_int_and_float)
+print(type(sum_int_and_float))
+
+#modulo 
+my_int_1 = 56
+my_int_2 = 12
+
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+mod_ints = my_int_1 % my_int_2
+mod_floats = my_float_2 % my_float_1
+
+print('Integer Modulo:', mod_ints) # Integer Modulo: 8
+print('Float Modulo:', mod_floats)
