5742f702fcbfac36160bdb02496124b3954a173b
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-16 15:35:34 +0300

    Updated numbers and operations in python

--- 01-basics/07_Numbers.py
+++ 01-basics/07_Numbers.py
@@ -6,4 +6,21 @@ my_int_2 = -4
 print(type(my_int_1))
 print(type(my_int_2))
 
+#addition of integers
 
+sum_ints = my_int_1 + my_int_2
+print('Integer Addition:', sum_ints) #Integer addition: 68
+
+#subtraction of ints
+my_int_1 = 56
+my_int_2 = 12
+
+diff_ints = my_int_1 - my_int_2
+print('Integer Subtraction:', diff_ints) # Integer Subtraction: 44
+
+#multiplication
+my_int_1 = 12
+my_int_2 = 4
+
+product_ints = my_int_1 * my_int_2
+print('Integer Multiplication:', product_ints)
