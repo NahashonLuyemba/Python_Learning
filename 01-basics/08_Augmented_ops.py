38943844b267969b3531389a9d74f539ace2ed6b
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-17 22:10:17 +0300

    update augmented ops

---
+++ 01-basics/08_Augmented_ops.py
@@ -0,0 +1,46 @@
+# Syntax = variable <operator>= value
+#addition assignment operator
+
+my_var = 10
+my_var += 5
+
+print(my_var)
+
+#same thing without augmented assignment
+
+my_var = 10
+my_var = my_var + 5
+
+print(my_var)
+
+#subtraction assignment
+count = 14
+count -= 3
+
+print(count)
+
+#multiplication assignment operator
+product = 65
+product *= 7
+
+print(product)
+
+#division assignment operator
+price = 100
+price /= 4
+
+print(price)
+
+#floor division operator
+total_pages = 23
+total_pages //= 5
+
+print(total_pages)
+
+#modulo assignment operator
+bits = 35
+bits %= 2
+
+print(bits) 
+
+
