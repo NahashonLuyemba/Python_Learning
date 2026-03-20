be4841c098c367b3b8332d7d167fbaa8d550382a
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-20 10:28:22 +0300

    Boolean and operator

---
+++ 01-basics/Booelan_Ops.py
@@ -0,0 +1,15 @@
+#And
+is_citizen = True
+age = 25
+
+print(is_citizen and age)
+
+#instead of nested if ststements
+is_citizen = True
+age = 25
+
+if is_citizen and age >= 18:
+    print('You are eligible to vote')
+else:
+    print('You are not eligible to vote')
+        
