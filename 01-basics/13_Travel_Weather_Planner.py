f10a092e1e83bf94b6372560389e124688f5cb6e
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-03-31 18:01:36 +0300

    Building weather travel planner

---
+++ 01-basics/13_Travel_Weather_Planner.py
@@ -0,0 +1,13 @@
+#Building a travel weather planner
+
+distance_mi = 50
+is_raining = True
+has_bike = True
+has_car = False
+has_ride_share_app = True
+
+if not distance_mi:
+    print("False")
+    
+
+
