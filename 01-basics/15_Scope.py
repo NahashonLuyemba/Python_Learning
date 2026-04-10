ab9418cc889a2e35964e0d9bb7214e355631731c
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-04-10 13:10:01 +0300

    Local scope

---
+++ 01-basics/15_Scope.py
@@ -0,0 +1,8 @@
+#In python scope determines the point a which you can access a variable.
+
+#Local scope , variable declared inside a function or class can olny be accessed from within that funct or class
+def my_func():
+    my_var = 10
+    print(my_var)
+
+my_func()
