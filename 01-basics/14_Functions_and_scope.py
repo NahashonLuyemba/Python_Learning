5e7e0b1c7b4667ba9e4a8211a77bb5d1574c609c
Nahashon Luyemba <nahashonluyemba10@gmail.com>
2026-04-10 12:06:04 +0300

    Use of special return keyword

--- 01-basics/14_Functions_and_scope.py
+++ 01-basics/14_Functions_and_scope.py
@@ -11,4 +11,13 @@ def hello():
 #simple function that prints the sum of two numbers.
 def calculate_sum(a,b):
     print(a + b)
-    
+
+#calculating sum of two numbers
+calculate_sum(3, 1)
+
+#functions also use a special "return" keyword to exit the function and return a value.
+def calculate_sum(a, b):
+    print(a + b)
+
+my_sum = calculate_sum(3, 1)
+print(my_sum)
